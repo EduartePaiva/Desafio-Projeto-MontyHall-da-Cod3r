289355a049641b613d21faeaecbc1268a07d000d
EduartePaiva <eduarte2018.po@gmail.com>
2023-03-25 15:38:02 -0300

    Criei o projeto
    Criei o projeto, adicionei o background geral, adicionei a porta e implementei a mecânica da porta aberta ou fechada

---
+++ monty-hall/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+ 
+  <div role="main" class='tela-inicial p-8 bg-gradient-to-r from-green-500 to-blue-500'>
+    
+    <input type="text">
+
+
+
+      <Porta :numPorta="1" ></Porta>
+    
+
+  </div>
+
+</template>
+
+<script setup lang="ts">
+  import Porta from './components/Porta.vue'
+
+
+
+</script>
+
+<style lang="scss" scoped>
+  .tela-inicial{
+    height: 100vh;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    gap: 32px;
+    flex-wrap: wrap;
+    overflow:auto;
+
+  }
+
+
+</style>
---
+++ monty-hall/src/components/Porta.vue
@@ -0,0 +1,79 @@
+<template>
+    <div @click="AbrirPorta" 
+        :class="portaAberta ? 'porta border-solid border-4 border-red-900 bg-black bg-opacity-50' : 'porta bg-amber-600 border-solid border-4 border-yellow-300'">
+
+
+        <div v-if="!portaAberta" class=" numero text-yellow-300 select-none text-center mt-3 text-2xl font-bold">
+             {{ numPorta }}
+        </div>
+        <div v-if="!portaAberta" class="macaneta bg-yellow-300 rounded-full ml-2"></div>
+        <div class="base bg-gray-300">
+        </div>
+
+    </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+    props: {
+        numPorta: Number,
+    },
+    
+    data(){
+        return {
+            portaAberta: false,
+        }
+    },
+    methods: {
+        AbrirPorta(){
+            this.portaAberta = !this.portaAberta
+        }
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+
+.portaEstaFechada{
+    height: 180px;
+    width: 100px;
+    min-width: 100px;
+    position: relative;
+    box-sizing: border-box;
+
+    border-bottom: none;
+}
+
+.porta{
+    height: 180px;
+    width: 100px;
+    min-width: 100px;
+    position: relative;
+    box-sizing: border-box;
+
+    border-bottom: none;
+    .macaneta{
+        width: 12px;
+        height: 12px;
+        top: 50%;
+        transform: translate(0,-50%);
+        position: absolute;
+    }
+
+    .base{
+        height: 4px;
+        width: 106px;
+        position: absolute;
+        z-index: 100;
+        top: 173px;
+        left: -7px;
+    }
+}
+
+
+
+</style>
