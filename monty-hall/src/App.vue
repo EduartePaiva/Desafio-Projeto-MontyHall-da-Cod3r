6e2f9861adec8525384d44da5f6684ee7dd2ebc1
EduartePaiva <eduarte2018.po@gmail.com>
2023-03-25 17:20:41 -0300

    logica de multiplas portas
    Implementado logica de multiplas portas

--- monty-hall/src/App.vue
+++ monty-hall/src/App.vue
@@ -1,36 +1,11 @@
 <template>
- 
-  <div role="main" class='tela-inicial p-8 bg-gradient-to-r from-green-500 to-blue-500'>
-    
-    <input type="text">
-
-
-
-      <Porta :numPorta="1" ></Porta>
-    
-
-  </div>
-
+ <Main/>
 </template>
 
 <script setup lang="ts">
-  import Porta from './components/Porta.vue'
-
-
+import Main from './main/Main.vue'
 
 </script>
 
 <style lang="scss" scoped>
-  .tela-inicial{
-    height: 100vh;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    gap: 32px;
-    flex-wrap: wrap;
-    overflow:auto;
-
-  }
-
-
 </style>
---
+++ monty-hall/src/main/Main.vue
@@ -0,0 +1,54 @@
+<template>
+    <div role="main" class='tela-inicial p-8 bg-gradient-to-r from-green-500 to-blue-500'>
+    
+
+    
+    <input v-model="novoItem" type="number" placeholder="Numéro de Portas">
+
+    <div v-for="portas in novoItem" :key="portas">
+        
+        <Porta :numPorta="portas" ></Porta>
+        
+    
+    </div>
+
+    
+
+  </div>
+</template>
+
+<script lang="ts">
+    import Porta from '../components/Porta.vue'
+    import { defineComponent } from 'vue'
+
+    export default defineComponent ({
+        components: { Porta },
+        props: {
+            novoItem: Number
+
+        },
+
+        data(){
+            return {
+                novoItem: 0
+            }
+        }
+
+    })
+
+
+</script>
+
+<style lang="scss" scoped>
+ .tela-inicial{
+    height: 100vh;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    gap: 32px;
+    flex-wrap: wrap;
+    overflow:auto;
+
+  }
+
+</style>
